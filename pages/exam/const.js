0a58171b97216232644c05d744a5317fdd438bb8
wuxue <wudaxue123@gmail.com>
2022-12-22 11:55:13 +0800

    init

--- pages/exam/const.js
+++
@@ -1,3 +0,0 @@
-export const EST_Single_Choice = 1 // 单选题
-export const EST_Multiple_Choice = 2 // 多选题
-export const EST_Judge = 3  // 判断题
--- pages/train/course/const.js
+++
@@ -1,5 +0,0 @@
-export const statusEnum  = {
-  1: '未学习',
-  2: '学习中',
-  3: '已完成',
-}
