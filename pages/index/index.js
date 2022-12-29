ca2c07d04cad9e7b78ccf306cc4839639869252f
wuxue <wudaxue123@gmail.com>
2022-12-29 17:20:43 +0800

    bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -9,8 +9,8 @@ Page({
     wx.navigateTo({ url: `/pages/login/login` })
   },
   toConfirm() {
-    if (!this.data.username) {
-      Toast('请输入姓名!')
+    if (!regex.test(this.data.username)) {
+      Toast.fail('请输入20个字以内的中英文字符、数字!')
       return
     }
     wx.navigateTo({
@@ -18,10 +18,6 @@ Page({
     })
   },
   onChanged({ detail }) {
-    if (!regex.test(detail.value)) {
-      Toast.fail('请输入20个字以内的中英文字符!')
-      return
-    }
     this.setData({ username: detail.value })
   }
 })
